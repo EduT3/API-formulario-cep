db77e1745c0460c320923cc270a5ab275889b64e
Eduardo Paulo Tomilhero <Eduardo_tomi@hotmail.com>
2023-06-01 12:31:58 -0300

    finalização

--- scripts.js
+++ scripts.js
@@ -1,13 +1,32 @@
-var consultaCEP = fetch("https://viacep.com.br/ws/06852430/json")
-  .then((resposta) => resposta.json())
-  .then((r) => {
-    if (r.erro) {
-      throw Error("Esse cep não existe!");
-    } else {
-      console.log(r);
-    }
-  })
-  .catch((erro) => console.log(erro))
-  .finally((mensagem) => console.log("Processamento concluído!"));
+async function buscaEndereco(cep) {
+  
+  var mensagemErro = document.getElementById('erro');
+  mensagemErro.innerHTML = "";
 
-console.log(consultaCEP);
+  try {
+    var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json`);
+    var consultaCEPConvertida = await consultaCEP.json();
+    if (consultaCEPConvertida.erro) {
+      throw Error("CEP não existente!");
+    }
+    var cidade = document.getElementById("cidade");
+    var logradouro = document.getElementById("endereco");
+    var estado = document.getElementById("estado");    
+    var bairro = document.getElementById("bairro");
+
+
+    cidade.value = consultaCEPConvertida.localidade;
+    logradouro.value = consultaCEPConvertida.logradouro;
+    estado.value = consultaCEPConvertida.uf;
+    bairro.value = consultaCEPConvertida.bairro;
+
+    console.log(consultaCEPConvertida);
+    return consultaCEPConvertida;
+  } catch (erro) {
+    mensagemErro.innerHTML= `<p>CEP inválido, Tente novamente!</p>`;
+    console.log(erro);
+  }
+}
+
+var cep = document.getElementById("cep");
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
